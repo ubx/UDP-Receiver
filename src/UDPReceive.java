d156569a66d89eaecba699ec76e3e89c67587bc5
ubx <andreas.luethi@gmx.net>
2013-11-12 22:01:16 +0100

    Add more output, prepare for SQLite.

--- src/UDPReceive.java
+++ src/UDPReceive.java
@@ -1,6 +1,9 @@
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.text.DecimalFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.TimeZone;
 
 public class UDPReceive {
 
@@ -8,6 +11,9 @@ public class UDPReceive {
     private static DecimalFormat dfLat = new DecimalFormat("##.######");
     private static DecimalFormat dfLon = new DecimalFormat("###.######");
     private static DecimalFormat dfAlt = new DecimalFormat("#####");
+    private static DecimalFormat dfMis = new DecimalFormat("####");
+
+    private static Calendar calendar = new GregorianCalendar(TimeZone.getTimeZone("GMT"));
 
     public static void main(String args[]) {
         try {
@@ -34,12 +40,22 @@ public class UDPReceive {
                     fix = Convert.convert(buffer);
                 }
                 if (fix != null) {
+                    int rcvtime = getCurrentTime();
                     System.out.println(packet.getAddress().getHostName()
-                            + ": Fix, time=" + fix.time
+                            + ": Fix, rcv time=" + rcvtime
+                            + " time=" + fix.time
+                            + "(" + (rcvtime - fix.time) + ")"
                             + " key=" + Convert.getHexKey(buffer)
                             + " lat=" + dfLat.format(fix.latitude)
                             + " lon=" + dfLon.format(fix.longitude)
-                            + " alt=" + dfAlt.format(fix.altitude));
+                            + " trk=" + dfMis.format(fix.track)
+                            + " gs=" + dfMis.format(fix.groundSpeed)
+                            + " alt=" + dfAlt.format(fix.altitude)
+                            + " vario=" + dfMis.format(fix.vario)
+                            + " noise=" + dfMis.format(fix.noise)
+                    );
+
+
                 } else {
                     // Convert the contents to a string, and display them
                     String msg = new String(buffer, 0, packet.getLength());
@@ -53,5 +69,14 @@ public class UDPReceive {
             System.err.println(e);
         }
     }
+
+
+    private static int getCurrentTime() {
+
+        calendar.setTimeInMillis(System.currentTimeMillis());
+        return (calendar.get(Calendar.HOUR_OF_DAY) * 3600 + calendar.get(Calendar.MINUTE) * 60
+                + calendar.get(Calendar.SECOND)) * 1000 + calendar.get(Calendar.MILLISECOND);
+    }
 }
+
            
